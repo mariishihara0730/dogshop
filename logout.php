00b96a35bef3a6906ee0501cd0c62af75ae71cbe
mariishihara0730 <41826065+mariishihara0730@users.noreply.github.com>
2020-03-15 16:03:59 +0900

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,18 @@
+<?php
+
+
+session_start();
+$session_name = session_name();
+$_SESSION = array();
+
+if (isset($_COOKIE[$session_name])) {
+    $params = session_get_cookie_params();
+    setcookie($session_name, '', time() - 42000,
+    $params["path"], $params["domain"],
+    $params["secure"], $params["httponly"]);
+}
+
+session_destroy();
+header('Location: login.php');
+exit;
+?>
